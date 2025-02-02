b505f2c9980f92791ae945fed25e7179ba596c60
Ankit Roy <68195611+royankit11@users.noreply.github.com>
2025-02-02 14:59:14 -0500

    Add files via upload

---
+++ Keepsake/KeepsakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepsakeApp.swift
+//  Keepsake
+//
+//  Created by Rik Roy on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct KeepsakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeepsakeTests/KeepsakeTests.swift
@@ -0,0 +1,17 @@
+//
+//  KeepsakeTests.swift
+//  KeepsakeTests
+//
+//  Created by Rik Roy on 2/2/25.
+//
+
+import Testing
+@testable import Keepsake
+
+struct KeepsakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
