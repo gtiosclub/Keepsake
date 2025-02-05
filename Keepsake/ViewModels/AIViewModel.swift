7785d3ba061bef7954626a8be1b3f9a794e336a2
mazam32 <hamdazam1@gmail.com>
2025-02-05 17:57:48 -0500

    Made a function to get image URLs using openAI API

--- Keepsake/ViewModels/AIViewModel.swift
+++ Keepsake/ViewModels/AIViewModel.swift
@@ -7,9 +7,21 @@
 
 import Foundation
 import ChatGPTSwift
+import UIKit
 
 class AIViewModel: ObservableObject {
-    var openAIAPIKey = ChatGPTAPI(apiKey: "<PUT API KEY HERE>")
+    var openAIAPIKey = ChatGPTAPI(apiKey: "")
     
+    func getImages(entry: JournalEntry) async throws -> String? {
+        var examplePrompt = ""
+        examplePrompt = entry.title + "\n" + entry.text
+        do {
+            let response = try await openAIAPIKey.generateDallE3Image(prompt: examplePrompt)
+            return response.url
+        } catch {
+            print("Error received: \(error)")
+            return nil
+        }
+    }
     
 }
--- KeepsakeTests/KeepsakeTests.swift
+++ KeepsakeTests/KeepsakeTests.swift
@@ -13,5 +13,11 @@ struct KeepsakeTests {
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
+    
+    @Test func getImages() async throws {
+        var journalEntry = JournalEntry(date: "01/01/2025", title: "Test",  text: "Test")
+        var aiViewModel: AIViewModel = AIViewModel()
+        await print(try aiViewModel.getImages(entry: journalEntry)!)
+    }
 
 }
