c474049772b02fd0125e2db52efad992591429c2
royankit11 <royankit11@gmail.com>
2025-02-05 15:50:21 -0500

    Added gpt package

---
+++ Keepsake/ViewModels/AIViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  AIViewModel.swift
+//  Keepsake
+//
+//  Created by Rik Roy on 2/5/25.
+//
+
+import Foundation
+import ChatGPTSwift
+
+class AIViewModel: ObservableObject {
+    var openAIAPIKey = ChatGPTAPI(apiKey: "<PUT API KEY HERE>")
+    
+    
+}
