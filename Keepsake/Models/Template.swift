5b0a19529ed4e8b06cb8631ec936a4e9937a4a3f
Alec Hance <alecnhance@gmail.com>
2025-02-05 11:03:16 -0500

    added some baseline models

---
+++ Keepsake/Models/Template.swift
@@ -0,0 +1,13 @@
+//
+//  Template.swift
+//  Keepsake
+//
+//  Created by Alec Hance on 2/5/25.
+//
+
+import Foundation
+
+struct Template {
+    var name: String
+    //insert other TBD variables like color, line type, etc
+}
