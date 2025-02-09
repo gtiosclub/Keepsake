cd508414738677a55901908ef55b03b7e74b0c94
mazam32 <hamdazam1@gmail.com>
2025-02-08 20:12:14 -0500

    AIView was added

--- Keepsake/ViewModels/AIViewModel.swift
+++ Keepsake/ViewModels/AIViewModel.swift
@@ -11,17 +11,31 @@ import UIKit
 
 class AIViewModel: ObservableObject {
     var openAIAPIKey = ChatGPTAPI(apiKey: "")
+    @Published var uiImage: UIImage? = nil
+    @Published var isLoading = false
     
-    func getImages(entry: JournalEntry) async throws -> String? {
-        var examplePrompt = ""
-        examplePrompt = entry.title + "\n" + entry.text
+    func generateImage(for entry: JournalEntry) async {
+        isLoading = true
+        defer{ isLoading = false }
+        
         do {
+            let examplePrompt = entry.title + "\n" + entry.text
             let response = try await openAIAPIKey.generateDallE3Image(prompt: examplePrompt)
-            return response.url
+            if let urlString = response.url {
+                self.uiImage = fetchUIImage(from: urlString)
+            }
         } catch {
-            print("Error received: \(error)")
-            return nil
+            print("Error: \(error)")
         }
+    }
+    
+    
+    func fetchUIImage(from urlString: String) -> UIImage? {
+        guard let url = URL(string: urlString), let data = try? Data(contentsOf: url) else {
+           print("Failed to load data from URL!")
+           return nil
+       }
+       return UIImage(data: data)
     }
     
 }
