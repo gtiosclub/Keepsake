1dd148eb49c8f0aa13cdef35564fb2bc9792a4f8
Alec Hance <alecnhance@gmail.com>
2025-02-10 17:25:03 -0500

    adding home page view to content view

--- Keepsake/ContentView.swift
+++ Keepsake/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            HomeView()
+            HomepageView()
                 .tabItem {
                     Label("Home", systemImage: "house")
                 }
