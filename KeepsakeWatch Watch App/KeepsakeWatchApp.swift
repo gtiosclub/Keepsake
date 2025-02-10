06361f82dde09785bf598a8369ea214d2082d36d
ishita-phoenix <129610304+ishita-phoenix@users.noreply.github.com>
2025-02-10 17:41:05 -0500

    voice recording view

--- KeepsakeWatch Watch App/KeepsakeWatchApp.swift
+++ KeepsakeWatch Watch App/KeepsakeWatchApp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct KeepsakeWatch_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
+            
             ContentView()
         }
     }
---
+++ KeepsakeWatch Watch App/VoiceRecording.swift
@@ -0,0 +1,25 @@
+//
+//  VoiceRecording.swift
+//  KeepsakeWatch Watch App
+//
+//  Created by Ishita on 2/10/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct VoiceRecordingView: View {
+    var body: some view {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    VoiceRecordingView()
+}
