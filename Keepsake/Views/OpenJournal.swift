426092e2e6c8b43f41402484c3e7cf1ec03bbbdc
Alec Hance <alecnhance@gmail.com>
2025-02-13 14:09:35 -0500

    upgrade to journal rings animation

--- Keepsake/Views/OpenJournal.swift
+++ Keepsake/Views/OpenJournal.swift
@@ -48,36 +48,54 @@ struct OpenJournal: View {
     @Binding var degrees: CGFloat
     @State var isHidden: Bool = false
     @Binding var show: Bool
+    @State var degreesPageOne: CGFloat = 0
+    @State var zIndex: Double = -1
+    @State var circleStart: CGFloat = 1
+    @State var circleEnd: CGFloat = 1
+    @State var animationColor: Color = .gray
     var body: some View {
         ZStack {
             VStack {
                 ForEach(0..<9, id: \.self) { i in
                     VStack(spacing: 0) {
-                        LeftSemi().stroke(Color.black, lineWidth: 2)
-                            .frame(width: UIScreen.main.bounds.width * 0.04, height: UIScreen.main.bounds.height * 0.02)
+                        HStack(spacing: 0) {
+                            LeftSemi().stroke(Color.clear, lineWidth: 2)
+                                .frame(width: UIScreen.main.bounds.width * 0.04, height: UIScreen.main.bounds.height * 0.02)
+                            RightSemi().stroke(Color.clear, lineWidth: 2)
+                                .frame(width: UIScreen.main.bounds.width * 0.04, height: UIScreen.main.bounds.height * 0.02)
+                        }
                         BottomLeftSemi().stroke(Color.black, lineWidth: 2)
-                            .frame(width: UIScreen.main.bounds.width * 0.04, height: UIScreen.main.bounds.height * 0.01)
+                            .frame(width: UIScreen.main.bounds.width * 0.08, height: UIScreen.main.bounds.height * 0.02)
                         
                     }
                     
                     
                 }
             }.offset(x: UIScreen.main.bounds.width * -0.45)
+                .zIndex(-100)
             RoundedRectangle(cornerRadius: 10)
                 .frame(width: UIScreen.main.bounds.width * 0.9 + 8, height: UIScreen.main.bounds.height * 0.55)
                 .foregroundStyle(book.template.coverColor)
                 .offset(x: 4, y: 7)
+                .zIndex(-4)
             RoundedRectangle(cornerRadius: 10)
                 .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.55)
+                .foregroundStyle(.yellow)
+                .offset(x: 5, y: 5)
+                .zIndex(-2)
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.55)
+                .zIndex(zIndex)
                 .foregroundStyle(book.template.pageColor)
                 .offset(x: 5, y: 5)
+                .rotation3DEffect(.degrees(degreesPageOne), axis: (x: 0.0, y: 1, z: 0.0), anchor: UnitPoint.leading, anchorZ: 0, perspective: 0.2)
             VStack {
                 ForEach(0..<9, id: \.self) { i in
                     VStack(spacing: 0) {
-                        RightSemi().stroke(Color.black, lineWidth: 2)
-                            .frame(width: UIScreen.main.bounds.width * 0.04, height: UIScreen.main.bounds.height * 0.02)
-                        BottomLeftSemi().stroke(Color.clear, lineWidth: 2)
-                            .frame(width: UIScreen.main.bounds.width * 0.04, height: UIScreen.main.bounds.height * 0.01)
+//                        RightSemi().stroke(Color.black, lineWidth: 2)
+//                            .frame(width: UIScreen.main.bounds.width * 0.04, height: UIScreen.main.bounds.height * 0.02)
+//                        BottomLeftSemi().stroke(Color.clear, lineWidth: 2)
+//                            .frame(width: UIScreen.main.bounds.width * 0.04, height: UIScreen.main.bounds.height * 0.01)
                     }
                     
                     
@@ -94,18 +112,47 @@ struct OpenJournal: View {
                 VStack {
                     ForEach(0..<9, id: \.self) { i in
                         VStack(spacing: 0) {
-                            RightSemi().stroke(Color.black, lineWidth: 2)
-                                .frame(width: UIScreen.main.bounds.width * 0.04, height: UIScreen.main.bounds.height * 0.02)
-                            BottomLeftSemi().stroke(Color.clear, lineWidth: 2)
-                                .frame(width: UIScreen.main.bounds.width * 0.04, height: UIScreen.main.bounds.height * 0.01)
+//                            RightSemi().stroke(Color.black, lineWidth: 2)
+//                                .frame(width: UIScreen.main.bounds.width * 0.04, height: UIScreen.main.bounds.height * 0.02)
+//                            BottomLeftSemi().stroke(Color.clear, lineWidth: 2)
+//                                .frame(width: UIScreen.main.bounds.width * 0.04, height: UIScreen.main.bounds.height * 0.01)
                             
                         }
                         
                         
                     }
                 }.offset(x: UIScreen.main.bounds.width * -0.43)
-                
             }.rotation3DEffect(.degrees(degrees), axis: (x: 0.0, y: 1, z: 0.0), anchor: UnitPoint.leading, anchorZ: 0, perspective: 0.2)
+                .zIndex(degrees == -180 ? -1 : 0)
+            VStack {
+                ForEach(0..<9, id: \.self) { i in
+                    VStack(spacing: 0) {
+                        ZStack {
+//                            HStack(spacing: 0) {
+//                                LeftSemi().stroke(Color.black, lineWidth: 2)
+//                                    .frame(width: UIScreen.main.bounds.width * 0.04, height: UIScreen.main.bounds.height * 0.02)
+//                                RightSemi().stroke(Color.black, lineWidth: 2)
+//                                    .frame(width: UIScreen.main.bounds.width * 0.04, height: UIScreen.main.bounds.height * 0.02)
+//                            }
+                            Circle()
+                                .trim(from: 0.5, to: 1)
+                                .stroke(lineWidth: 2)
+                                .frame(width: UIScreen.main.bounds.width * 0.08)
+                            Circle()
+                                .trim(from: circleStart, to: circleEnd)
+                                .stroke(animationColor, lineWidth: 3)
+                                .frame(width: UIScreen.main.bounds.width * 0.08)
+                            
+                            BottomLeftSemi().stroke(Color.clear, lineWidth: 2)
+                                .frame(width: UIScreen.main.bounds.width * 0.08, height: UIScreen.main.bounds.height * 0.02)
+                                .offset(y: -UIScreen.main.bounds.height * 0.02)
+                        }.frame(width: UIScreen.main.bounds.width * 0.08, height: UIScreen.main.bounds.height * 0.04)
+                        
+                    }
+                    
+                    
+                }
+            }.offset(x: UIScreen.main.bounds.width * -0.45)
             HStack {
                 Button("Back") {
                     withAnimation(.linear(duration: 1).delay(0.5)) {
@@ -117,10 +164,31 @@ struct OpenJournal: View {
                     }
                 }
                 Button("Page left") {
-                    
+                    zIndex = -0.5
+                    withAnimation(.linear(duration: 1).delay(0.5)) {
+                        degreesPageOne -= 90
+                        circleStart -= 0.25
+                    } completion: {
+                        circleStart = 0.5
+                        circleEnd = 0.75
+                        withAnimation(.linear(duration: 1).delay(0)) {
+                            degreesPageOne -= 90
+                            circleEnd -= 0.25
+                        }
+                    }
                 }
                 Button("Page Right"){
-                    
+                    withAnimation(.linear(duration: 1).delay(0.5)) {
+                        degreesPageOne += 90
+                        circleEnd += 0.25
+                    } completion: {
+                        circleStart = 0.75
+                        circleEnd = 1
+                        withAnimation(.linear(duration: 1).delay(0)) {
+                            degreesPageOne += 90
+                            circleStart += 0.25
+                        }
+                    }
                 }
                 
             }.offset(y: 280)
@@ -134,10 +202,10 @@ struct OpenJournal: View {
 
 #Preview {
     struct Preview: View {
-        @State var number: CGFloat = -180.0
+        @State var number: CGFloat = -180
         @State var show: Bool = true
         var body: some View {
-            OpenJournal(book: Journal(name: "Journal 1", createdDate: "2/2/25", entries: [], category: "entry1", isSaved: true, isShared: false, template: Template(coverColor: .red, pageColor: .white, titleColor: .black)), degrees: $number, show: $show)
+            OpenJournal(book: Journal(name: "Journal 1", createdDate: "2/2/25", entries: [], category: "entry1", isSaved: true, isShared: false, template: Template(coverColor: .red, pageColor: .gray, titleColor: .black)), degrees: $number, show: $show)
         }
     }
 
