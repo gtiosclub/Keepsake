14131c96f7e6ce1e9c2b983e0e88bf6953e160b4
Alec Hance <alecnhance@gmail.com>
2025-02-13 22:20:30 -0500

    page swiping fixes

--- Keepsake/Views/HomepageView.swift
+++ Keepsake/Views/HomepageView.swift
@@ -10,9 +10,15 @@ struct HomepageView: View {
     @Namespace private var shelfNamespace
     var shelf: Shelf
     @State var degrees: CGFloat = 0
+    @State var frontDegrees: CGFloat = 0
     @State var show: Bool = false
     @State var number = 0
     @State var bind: Int?
+    @State var coverZ: Double = 0
+    @State var circleStart: CGFloat = 0.5
+    @State var circleEnd: CGFloat = 0.5
+    @State var ellipseStart: CGFloat = 1
+    @State var ellipseEnd: CGFloat = 1
 //    @State var isHidden: Bool = false
 //    @State var selectedBook: Int = -1
     
@@ -83,10 +89,23 @@ struct HomepageView: View {
             }
             .frame(maxHeight: .infinity, alignment: .top)
         } else {
-            OpenJournal(book: (shelf.books[number] as? Journal)!, degrees: $degrees, show: $show, displayPageIndex: 0).matchedGeometryEffect(id: "journal_\(number)", in: shelfNamespace, properties: .position, anchor: .center)
+            OpenJournal(book: (shelf.books[number] as? Journal)!, degrees: $degrees, show: $show, frontDegrees: $frontDegrees, circleStart: $circleStart, circleEnd: $circleEnd, displayPageIndex: 0, coverZ: $coverZ).matchedGeometryEffect(id: "journal_\(number)", in: shelfNamespace, properties: .position, anchor: .center)
                 .onAppear() {
-                    withAnimation(.linear(duration: 1).delay(0.7)) {
-                        degrees = -180
+                    circleStart = 1
+                    circleEnd = 1
+                    withAnimation(.linear(duration: 0.7).delay(0.7)) {
+                        circleStart -= 0.25
+                        degrees -= 90
+                        frontDegrees -= 90
+                    } completion: {
+                        circleStart = 0.5
+                        circleEnd = 0.75
+                        coverZ = -1
+                        withAnimation(.linear(duration: 0.7).delay(0)) {
+                            circleEnd -= 0.25
+                            degrees -= 90
+                            frontDegrees -= 90
+                        }
                     }
                 }
         }
@@ -104,9 +123,9 @@ struct HomepageView: View {
 
 #Preview {
     HomepageView(shelf: Shelf(name: "Bookshelf", books: [
-        Journal(name: "Journal 1", createdDate: "2/2/25", entries: [], category: "entry1", isSaved: true, isShared: false, template: Template(coverColor: .red, pageColor: .white, titleColor: .black), pages: []),
-        Journal(name: "Journal 2", createdDate: "2/3/25", entries: [], category: "entry2", isSaved: true, isShared: true, template: Template(coverColor: .green, pageColor: .white, titleColor: .black), pages: []),
-        Journal(name: "Journal 3", createdDate: "2/4/25", entries: [], category: "entry3", isSaved: false, isShared: false, template: Template(coverColor: .blue, pageColor: .black, titleColor: .white), pages: []),
-        Journal(name: "Journal 4", createdDate: "2/5/25", entries: [], category: "entry4", isSaved: true, isShared: false, template: Template(coverColor: .brown, pageColor: .white, titleColor: .black), pages: [])
+        Journal(name: "Journal 1", createdDate: "2/2/25", entries: [], category: "entry1", isSaved: true, isShared: false, template: Template(coverColor: .red, pageColor: Color(red: 0.9, green: 0.9, blue: 0.9), titleColor: .black), pages: [JournalPage(number: 1, entries: []), JournalPage(number: 2, entries: []), JournalPage(number: 3, entries: []), JournalPage(number: 4, entries: []), JournalPage(number: 5, entries: [])]),
+        Journal(name: "Journal 2", createdDate: "2/3/25", entries: [], category: "entry2", isSaved: true, isShared: true, template: Template(coverColor: .green, pageColor: .white, titleColor: .black), pages: [JournalPage(number: 1, entries: []), JournalPage(number: 2, entries: []), JournalPage(number: 3, entries: []), JournalPage(number: 4, entries: []), JournalPage(number: 5, entries: [])]),
+        Journal(name: "Journal 3", createdDate: "2/4/25", entries: [], category: "entry3", isSaved: false, isShared: false, template: Template(coverColor: .blue, pageColor: .black, titleColor: .white), pages: [JournalPage(number: 1, entries: []), JournalPage(number: 2, entries: []), JournalPage(number: 3, entries: []), JournalPage(number: 4, entries: []), JournalPage(number: 5, entries: [])]),
+        Journal(name: "Journal 4", createdDate: "2/5/25", entries: [], category: "entry4", isSaved: true, isShared: false, template: Template(coverColor: .brown, pageColor: .white, titleColor: .black), pages: [JournalPage(number: 1, entries: []), JournalPage(number: 2, entries: []), JournalPage(number: 3, entries: []), JournalPage(number: 4, entries: []), JournalPage(number: 5, entries: [])])
     ]))
 }
--- Keepsake/Views/OpenJournal.swift
+++ Keepsake/Views/OpenJournal.swift
@@ -13,33 +13,25 @@ struct LeftSemi: Shape {
         path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
         path.addQuadCurve(to: CGPoint(x: rect.maxX, y: rect.minY),
                           control: CGPoint(x: rect.minX, y:  rect.minY))
-        
         return path
-
     }
 }
-
 struct RightSemi: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
         path.move(to: CGPoint(x: rect.maxX, y: rect.maxY))
         path.addQuadCurve(to: CGPoint(x: rect.minX, y: rect.minY),
                           control: CGPoint(x: rect.maxX, y:  rect.minY))
-        
         return path
-
     }
 }
-
 struct BottomLeftSemi: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
         path.move(to: CGPoint(x: rect.minX, y: rect.minY))
         path.addQuadCurve(to: CGPoint(x: rect.maxX, y: rect.maxY),
                           control: CGPoint(x: rect.minX, y:  rect.maxY))
-        
         return path
-
     }
 }
 
@@ -50,13 +42,14 @@ struct OpenJournal: View {
     @State var isHidden: Bool = false
     @Binding var show: Bool
     @State var displayDegrees: CGFloat = 0
-    @State var frontDegrees: CGFloat = -180
+    @Binding var frontDegrees: CGFloat
     @State var zIndex: Double = -1
-    @State var circleStart: CGFloat = 1
-    @State var circleEnd: CGFloat = 1
+    @Binding var circleStart: CGFloat
+    @Binding var circleEnd: CGFloat
     @State var displayPageIndex: Int
     @State var displayIsHidden: Bool = false
     @State var frontIsHidden: Bool = true
+    @Binding var coverZ: Double
     var body: some View {
         ZStack {
             VStack {
@@ -64,22 +57,21 @@ struct OpenJournal: View {
                     VStack(spacing: 0) {
                         BottomLeftSemi().stroke(Color.black, lineWidth: 2)
                             .frame(width: UIScreen.main.bounds.width * 0.08, height: UIScreen.main.bounds.height * 0.02).padding(.top, UIScreen.main.bounds.height * 0.02)
-                        
                     }
                 }
             }.offset(x: UIScreen.main.bounds.width * -0.45)
                 .zIndex(-100)
             RoundedRectangle(cornerRadius: 10)
-                .frame(width: UIScreen.main.bounds.width * 0.9 + 8, height: UIScreen.main.bounds.height * 0.55)
+                .frame(width: UIScreen.main.bounds.width * 0.9 + 8, height: UIScreen.main.bounds.height * 0.56)
                 .foregroundStyle(book.template.coverColor)
-                .offset(x: 4, y: 7)
+                .offset(x: UIScreen.main.bounds.height * 0.004, y: 0)
                 .zIndex(-4)
             //Back Page
             ZStack {
                 RoundedRectangle(cornerRadius: 10)
                     .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.55)
                     .foregroundStyle(book.template.pageColor)
-                    .offset(x: 5, y: 5)
+                    .offset(x: UIScreen.main.bounds.height * 0.004, y: 0)
                     .zIndex(-2)
                 Text(displayPageIndex + 1 < book.pages.count && displayPageIndex + 1 > -1 ? "\(book.pages[displayPageIndex + 1].number)" : "no more pages")
             }
@@ -87,39 +79,41 @@ struct OpenJournal: View {
             ZStack {
                 RoundedRectangle(cornerRadius: 10)
                     .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.55)
-                    .zIndex(zIndex)
+                    .zIndex(displayIsHidden ? 0 : zIndex)
                     .foregroundStyle(book.template.pageColor)
-                    .offset(x: 5, y: 5)
+                    .offset(x: 5, y: 0)
                 Text(displayPageIndex < book.pages.count && displayPageIndex > -1 ? "\(book.pages[displayPageIndex].number)" : "no more pages")
-                    .opacity(displayIsHidden ? 0 : 1)
+
             }.rotation3DEffect(.degrees(displayDegrees), axis: (x: 0.0, y: 1, z: 0.0), anchor: UnitPoint.leading, anchorZ: 0, perspective: 0.2)
+              
             //Fake Front Page
             RoundedRectangle(cornerRadius: 10)
                 .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.55)
                 .zIndex(0)
                 .foregroundStyle(book.template.pageColor)
-                .offset(x: 5, y: 5)
-                .rotation3DEffect(.degrees(-180), axis: (x: 0.0, y: 1, z: 0.0), anchor: UnitPoint.leading, anchorZ: 0, perspective: 0.2)
+                .offset(x: UIScreen.main.bounds.height * 0.004, y: 0)
+                .rotation3DEffect(.degrees(degrees), axis: (x: 0.0, y: 1, z: 0.0), anchor: UnitPoint.leading, anchorZ: 0, perspective: 0.2)
             //Front Page
             ZStack {
                 RoundedRectangle(cornerRadius: 10)
                     .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.55)
                     .zIndex(0)
                     .foregroundStyle(book.template.pageColor)
-                    .offset(x: 5, y: 5)
+                    .offset(x: UIScreen.main.bounds.height * 0.004, y: 0)
                 Text(displayPageIndex - 1 < book.pages.count && displayPageIndex - 1 > -1 ? "\(book.pages[displayPageIndex - 1].number)" : "no more pages")
-                    .opacity(frontIsHidden ? 0 : 1)
+//                    .opacity(frontIsHidden ? 0 : 1)
             }.rotation3DEffect(.degrees(frontDegrees), axis: (x: 0.0, y: 1, z: 0.0), anchor: UnitPoint.leading, anchorZ: 0, perspective: 0.2)
+            //Cover page
             ZStack {
                 RoundedRectangle(cornerRadius: 10)
-                    .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.55)
+                    .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.56)
                     .foregroundStyle(book.template.coverColor)
                 Text(book.name)
                     .font(.title)
                     .foregroundStyle(book.template.titleColor)
-                    .opacity(isHidden ? 0 : 1)
+//                    .opacity(isHidden ? 0 : 1)
             }.rotation3DEffect(.degrees(degrees), axis: (x: 0.0, y: 1, z: 0.0), anchor: UnitPoint.leading, anchorZ: 0, perspective: 0.2)
-                .zIndex(degrees == -180 ? -1 : 0)
+                .zIndex(coverZ)
             VStack {
                 ForEach(0..<9, id: \.self) { i in
                     VStack(spacing: 0) {
@@ -130,7 +124,7 @@ struct OpenJournal: View {
                                 .frame(width: UIScreen.main.bounds.width * 0.08)
                             Circle()
                                 .trim(from: circleStart, to: circleEnd)
-                                .stroke(book.template.pageColor, lineWidth: 3)
+                                .stroke(coverZ != 0 ? book.template.pageColor : book.template.coverColor, lineWidth: 3)
                                 .frame(width: UIScreen.main.bounds.width * 0.08)
                             
                             BottomLeftSemi().stroke(Color.clear, lineWidth: 2)
@@ -145,11 +139,24 @@ struct OpenJournal: View {
             }.offset(x: UIScreen.main.bounds.width * -0.45)
             HStack {
                 Button("Back") {
+                    circleStart = 0.5
+                    circleEnd = 0.5
                     withAnimation(.linear(duration: 1).delay(0.5)) {
-                        degrees += 180
+                        circleEnd += 0.25
+                        degrees += 90
+                        frontDegrees += 90
                     } completion: {
+                        circleStart = 0.75
+                        circleEnd = 1
+                        coverZ = 0
                         withAnimation {
-                            show.toggle()
+                            circleStart += 0.25
+                            degrees += 90
+                            frontDegrees += 90
+                        } completion: {
+                            withAnimation(.linear(duration: 1).delay(0.5)) {
+                                show.toggle()
+                            }
                         }
                     }
                 }
@@ -157,14 +164,14 @@ struct OpenJournal: View {
                     circleStart = 1
                     circleEnd = 1
                     zIndex = -0.5
-                    withAnimation(.linear(duration: 1).delay(0.5)) {
+                    withAnimation(.linear(duration: 0.5).delay(0.5)) {
                         displayDegrees -= 90
                         circleStart -= 0.25
                     } completion: {
                         circleStart = 0.5
                         circleEnd = 0.75
                         displayIsHidden = true
-                        withAnimation(.linear(duration: 1).delay(0)) {
+                        withAnimation(.linear(duration: 0.5).delay(0)) {
                             displayDegrees -= 90
                             circleEnd -= 0.25
                         } completion: {
@@ -177,14 +184,14 @@ struct OpenJournal: View {
                 Button("Page Right"){
                     circleStart = 0.5
                     circleEnd = 0.5
-                    withAnimation(.linear(duration: 1).delay(0.5)) {
+                    withAnimation(.linear(duration: 0.5).delay(0.5)) {
                         frontDegrees += 90
                         circleEnd += 0.25
                     } completion: {
                         frontIsHidden = false
                         circleStart = 0.75
                         circleEnd = 1
-                        withAnimation(.linear(duration: 1).delay(0)) {
+                        withAnimation(.linear(duration: 0.5).delay(0)) {
                             frontDegrees += 90
                             circleStart += 0.25
                         } completion: {
@@ -207,9 +214,13 @@ struct OpenJournal: View {
 #Preview {
     struct Preview: View {
         @State var number: CGFloat = -180
+        @State var number2: CGFloat = -180
         @State var show: Bool = true
+        @State var cover: Double = -2
+        @State var circleStart: CGFloat = 1
+        @State var circleEnd: CGFloat = 1
         var body: some View {
-            OpenJournal(book: Journal(name: "Journal 1", createdDate: "2/2/25", entries: [], category: "entry1", isSaved: true, isShared: false, template: Template(coverColor: .red, pageColor: .gray, titleColor: .black), pages: [JournalPage(number: 1, entries: []), JournalPage(number: 2, entries: []), JournalPage(number: 3, entries: []), JournalPage(number: 4, entries: []), JournalPage(number: 5, entries: [])]), degrees: $number, show: $show, displayPageIndex: 2)
+            OpenJournal(book: Journal(name: "Journal 1", createdDate: "2/2/25", entries: [], category: "entry1", isSaved: true, isShared: false, template: Template(coverColor: .red, pageColor: .gray, titleColor: .black), pages: [JournalPage(number: 1, entries: []), JournalPage(number: 2, entries: []), JournalPage(number: 3, entries: []), JournalPage(number: 4, entries: []), JournalPage(number: 5, entries: [])]), degrees: $number, show: $show, frontDegrees: $number2, circleStart: $circleStart, circleEnd: $circleEnd, displayPageIndex: 2, coverZ: $cover)
         }
     }
 
