a723ed44c2c175234879fc90169334e1f6dae289
Connor-1233 <Connor.e.case@gmail.com>
2025-02-19 17:45:48 -0500

    Changing Init Function for Template Model

--- Keepsake/Models/Template.swift
+++ Keepsake/Models/Template.swift
@@ -14,4 +14,18 @@ struct Template {
     var pageColor: Color
     var titleColor: Color
     //insert other TBD variables like color, line type, etc
+//    
+    init(name: String, coverColor: Color, pageColor: Color, titleColor: Color) {
+        self.name = name
+        self.coverColor = coverColor
+        self.pageColor = pageColor
+        self.titleColor = titleColor
+    }
+    
+    init() {
+        self.name = "Default"
+        self.coverColor = .blue
+        self.pageColor = .white
+        self.titleColor = .black
+    }
 }
