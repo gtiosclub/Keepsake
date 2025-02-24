2b8940e13db6aa0a7f8bc4a30dd015b38cb15622
Connor-1233 <Connor.e.case@gmail.com>
2025-02-24 17:34:58 -0500

    Cleaning up LibraryView

--- Keepsake/Views/LibraryView.swift
+++ Keepsake/Views/LibraryView.swift
@@ -9,7 +9,6 @@
 import SwiftUI
 
 struct LibraryView: View {
-//    let bookshelves = ["2025", "2024", "2023", "2022"]
     @ObservedObject var user: User
     var body: some View {
         NavigationView {
@@ -23,7 +22,6 @@ struct LibraryView: View {
                 .frame(maxWidth: .infinity)
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
-//                .background(Color.gray.opacity(0.1))
             .navigationTitle("Your Library")
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
@@ -39,7 +37,7 @@ struct LibraryView: View {
 struct LibraryView_Previews: PreviewProvider {
     static var previews: some View {
         let sampleUser = User(id: "1", name: "Sample User", shelves: [
-            Shelf(name: "Bookshelf", books: [
+            Shelf(name: "2025", books: [
                 Journal(name: "Journal 1", createdDate: "2/2/25", entries: [], category: "entry1", isSaved: true, isShared: false, template: Template(name: "Template 1", coverColor: .red, pageColor: Color(red: 0.9, green: 0.9, blue: 0.9), titleColor: .black), pages: [JournalPage(number: 1, entries: []), JournalPage(number: 2, entries: []), JournalPage(number: 3, entries: []), JournalPage(number: 4, entries: []), JournalPage(number: 5, entries: [])]),
                 Journal(name: "Journal 2", createdDate: "2/3/25", entries: [], category: "entry2", isSaved: true, isShared: true, template: Template(name: "Template 2", coverColor: .green, pageColor: .white, titleColor: .black), pages: [JournalPage(number: 1, entries: []), JournalPage(number: 2, entries: []), JournalPage(number: 3, entries: []), JournalPage(number: 4, entries: []), JournalPage(number: 5, entries: [])]),
                 Journal(name: "Journal 3", createdDate: "2/4/25", entries: [], category: "entry3", isSaved: false, isShared: false, template: Template(name: "Template 3", coverColor: .blue, pageColor: .black, titleColor: .white), pages: [JournalPage(number: 1, entries: []), JournalPage(number: 2, entries: []), JournalPage(number: 3, entries: []), JournalPage(number: 4, entries: []), JournalPage(number: 5, entries: [])]),
