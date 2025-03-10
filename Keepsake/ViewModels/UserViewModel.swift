969df7cc3dc324e05fa5e062914e7faa6dff2e9e
Alec Hance <alecnhance@gmail.com>
2025-03-10 13:13:57 -0400

    updated add entry in userVM

--- Keepsake/ViewModels/UserViewModel.swift
+++ Keepsake/ViewModels/UserViewModel.swift
@@ -38,9 +38,29 @@ class UserViewModel: ObservableObject {
         var page = user.getJournalShelves()[shelfIndex].journals[bookIndex].pages[pageNum]
         var entries = page.entries
         switch page.realEntryCount {
-        case 0: {
-            
+        case 0:
+            entries[0] = JournalEntry(entry: entry, width: 2, height: 2, color: entry.color)
+        case 1:
+            entries[4] = JournalEntry(entry: entry, width: 2, height: 2, color: entry.color)
+        case 2:
+            entries[0] = JournalEntry(entry: entries[0], width: 1, height: 2, color: entries[0].color)
+            entries[1] = JournalEntry(entry: entry, width: 1, height: 2, color: entry.color)
+        case 3:
+            entries[1] = JournalEntry(entry: entries[2], width: 1, height: 1, color: entries[0].color)
+            entries[3] = JournalEntry(entry: entry, width: 1, height: 1, color: entry.color)
+        case 4:
+            entries[4] = JournalEntry(entry: entries[4], width: 2, height: 1, color: entries[4].color)
+            entries[6] = JournalEntry(entry: entry, width: 2, height: 1, color: entry.color)
+        case 5:
+            entries[6] = JournalEntry(entry: entries[6], width: 1, height: 1, color: entries[6].color)
+            entries[7] = JournalEntry(entry: entry, width: 1, height: 1, color: entry.color)
+        case 6:
+            entries[4] = JournalEntry(entry: entries[4], width: 1, height: 1, color: entries[4].color)
+            entries[5] = JournalEntry(entry: entry, width: 1, height: 1, color: entry.color)
+        default:
+            entries[0] = JournalEntry(entry: entries[0], width: 1, height: 1, color: entries[0].color)
+            entries[2] = JournalEntry(entry: entry, width: 1, height: 1, color: entries[0].color)
         }
-        }
+        page.realEntryCount += 1
     }
 }
