b8c4e14c405fd64f29fb4580655d2ca0540572e0
lunanith <ravula.nithya@gmail.com>
2025-03-24 17:58:47 -0400

    content view on profile

--- Keepsake/ContentView.swift
+++ Keepsake/ContentView.swift
@@ -13,27 +13,29 @@ struct ContentView: View {
     @ObservedObject var fbVM: FirebaseViewModel
 
     var body: some View {
-        TabView {
-            Tab("Home", systemImage: "house") {
-                LibraryView(userVM: userVM, aiVM: aiVM, fbVM: fbVM)
+        NavigationView {
+            TabView {
+                Tab("Home", systemImage: "house") {
+                    LibraryView(userVM: userVM, aiVM: aiVM, fbVM: fbVM)
+                }
+                
+                
+                Tab("Community", systemImage: "person.2") {
+                    CommunityView()
+                }
+                Tab("Scrapbooks", systemImage: "ellipsis.viewfinder") {
+                    ScrapbookView()
+                }
+            }.onAppear {
+                // correct the transparency bug for Tab bars
+                let tabBarAppearance = UITabBarAppearance()
+                tabBarAppearance.configureWithOpaqueBackground()
+                UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+                // correct the transparency bug for Navigation bars
+                let navigationBarAppearance = UINavigationBarAppearance()
+                navigationBarAppearance.configureWithOpaqueBackground()
+                UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
             }
-
-
-            Tab("Community", systemImage: "person.2") {
-                CommunityView()
-            }
-            Tab("Scrapbooks", systemImage: "ellipsis.viewfinder") {
-                ScrapbookView()
-            }
-        }.onAppear {
-            // correct the transparency bug for Tab bars
-            let tabBarAppearance = UITabBarAppearance()
-            tabBarAppearance.configureWithOpaqueBackground()
-            UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
-            // correct the transparency bug for Navigation bars
-            let navigationBarAppearance = UINavigationBarAppearance()
-            navigationBarAppearance.configureWithOpaqueBackground()
-            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
         }
     }
 }
