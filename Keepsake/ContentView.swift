c2039a25ba9df09e8fbdfd3a16e008b7deb49b11
NityaPotti <126093831+NityaPotti@users.noreply.github.com>
2025-03-26 11:41:57 -0400

    Revert "content view on profile"

--- Keepsake/ContentView.swift
+++ Keepsake/ContentView.swift
@@ -13,31 +13,27 @@ struct ContentView: View {
     @ObservedObject var fbVM: FirebaseViewModel
 
     var body: some View {
-
-        NavigationView {
-            TabView {
-                Tab("Home", systemImage: "house") {
-                    LibraryView(userVM: userVM, aiVM: aiVM, fbVM: fbVM)
-                }
-                
-                
-                Tab("Community", systemImage: "person.2") {
-                    CommunityView()
-                }
-                Tab("Scrapbooks", systemImage: "ellipsis.viewfinder") {
-                    ScrapbookView()
-                }
-            }.onAppear {
-                // correct the transparency bug for Tab bars
-                let tabBarAppearance = UITabBarAppearance()
-                tabBarAppearance.configureWithOpaqueBackground()
-                UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
-                // correct the transparency bug for Navigation bars
-                let navigationBarAppearance = UINavigationBarAppearance()
-                navigationBarAppearance.configureWithOpaqueBackground()
-                UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
-
+        TabView {
+            Tab("Home", systemImage: "house") {
+                HomeView(userVM: userVM, aiVM: aiVM, fbVM: fbVM, selectedOption: .journal_shelf)
             }
+
+
+            Tab("Community", systemImage: "person.2") {
+                CommunityView()
+            }
+            Tab("Scrapbooks", systemImage: "ellipsis.viewfinder") {
+                ScrapbookView()
+            }
+        }.onAppear {
+            // correct the transparency bug for Tab bars
+            let tabBarAppearance = UITabBarAppearance()
+            tabBarAppearance.configureWithOpaqueBackground()
+            UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+            // correct the transparency bug for Navigation bars
+            let navigationBarAppearance = UINavigationBarAppearance()
+            navigationBarAppearance.configureWithOpaqueBackground()
+            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
         }
     }
 }
