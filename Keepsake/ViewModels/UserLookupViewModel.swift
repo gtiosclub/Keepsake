22056262e7629a4656c7d423d6cd802fb37e7480
Nithya <52676594+lunanith@users.noreply.github.com>
2025-03-29 12:03:38 -0400

    search for users

---
+++ Keepsake/ViewModels/UserLookupViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserLookupViewModel.swift
+//  Keepsake
+//
+//  Created by Nithya Ravula on 3/28/25.
+//
+
+import Foundation
---
+++ Keepsake/Views/UserSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  UserSearchView.swift
+//  Keepsake
+//
+//  Created by Nithya Ravula on 3/28/25.
+//
+
+import SwiftUI
+
+struct UserSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserSearchView()
+}
